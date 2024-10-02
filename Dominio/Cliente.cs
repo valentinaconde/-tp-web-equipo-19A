50c1cefccc1ee15c4afa6170eea0721a797addd8
milidub <milagrosdubuis@hotmail.com>
2024-10-02 15:13:57 -0300

    detalles dominio

--- Dominio/Cliente.cs
+++ Dominio/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ namespace Dominio
         public string Email { get; set; }
         public string Direccion { get; set; }
         public string Ciudad { get; set; }
+        [DisplayName("Codigo postal")]
         public int CP { get; set; }
 
     }
